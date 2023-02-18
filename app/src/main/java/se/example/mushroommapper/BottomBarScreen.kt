615b0c4083589518b58887fe2708b9af78c25645
foal20ym <alexander-forsanker2011@hotmail.com>
2023-02-18 21:16:57 +0100

    Implemented the Navigation structure, launch screen & home screen.

---
+++ app/src/main/java/se/example/mushroommapper/BottomBarScreen.kt
@@ -0,0 +1,38 @@
+package se.example.mushroommapper
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home : BottomBarScreen(
+        route = "HOME",
+        title = "HOME",
+        icon = Icons.Default.Home
+    )
+
+    object Profile : BottomBarScreen(
+        route = "PROFILE",
+        title = "PROFILE",
+        icon = Icons.Default.Person
+    )
+
+    object Map : BottomBarScreen(
+        route = "MAP",
+        title = "MAP",
+        icon = Icons.Default.Place
+    )
+
+    object Settings : BottomBarScreen(
+        route = "SETTINGS",
+        title = "SETTINGS",
+        icon = Icons.Default.Settings
+    )
+}
