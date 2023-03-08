14c5b03ae94302039e7bd3b72b8dce3d1f122740
foal20ym <alexander-forsanker2011@hotmail.com>
2023-03-08 10:47:47 +0100

    It is now possible to see each users added locations on the map.

--- app/src/main/java/se/example/mushroommapper/view/MapScreen.kt
+++ app/src/main/java/se/example/mushroommapper/view/MapScreen.kt
@@ -2,8 +2,9 @@ package se.example.mushroommapper.view
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -11,27 +12,37 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import se.example.mushroommapper.model.Places
-import se.example.mushroommapper.viewModel.DataViewModel
-import se.example.mushroommapper.viewModel.MapViewModel
+import se.example.mushroommapper.viewModel.HomeViewModel
 
 
 @Composable
 fun MapScreen(
-    dataViewModel: DataViewModel = viewModel(),
+    homeViewModel: HomeViewModel,
 ) {
+
+    val homeUIState = homeViewModel.homeUIState
+    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = Unit){
+        homeViewModel.loadPlaces()
+    }
+
+    val places = mutableListOf<Places>()
+
+    for(place in homeUIState.placesList.data ?: emptyList()){
+        places.add(Places(place.userId,place.title, place.description,place.latitude,place.longitude))
+    }
+    //val mostRecentPosition
+
     val singapore = LatLng(1.35, 103.87)
-    val l1 = "1.30"
+    /*val l1 = "1.30"
     val l2 = "103.84"
     val places = listOf(
         Places("Singapore","Nice city", "cool city",1.36,103.87),
         Places("Singapore2","Nice city", "cool city",1.39,103.87),
         Places("Singapore3","Nice city", "cool city",1.42,103.87),
         Places("Singapore4","Nice city", "cool city",l1.toDouble(),l2.toDouble()),
-    )
-
-    val getData = dataViewModel.state.value
-    val locationn = LatLng(getData.latitude, getData.longitude)
-    val places2 = getData
+    )*/
 
 
     val cameraPositionState = rememberCameraPositionState {
