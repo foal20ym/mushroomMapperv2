892d725010b32d564b915e56427326d254628833
John Wennerberg <johnwennerberg@Johns-MacBook-Pro.local>
2023-03-08 10:48:30 +0100

    Reworked UI for login page

--- app/src/main/java/se/example/mushroommapper/ui/theme/Color.kt
+++ app/src/main/java/se/example/mushroommapper/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+const val BACKGROUND_COLOR = "#1d2129"
+const val INTERACTABLE_COLOR = "#AB4C4C"
+const val NON_INTERACTABLE_COLOR = "#ffffff"
