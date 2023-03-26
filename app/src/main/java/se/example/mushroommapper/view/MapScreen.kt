26beb0be3ada26831459706ae038f695f7f1650f
Alexander Forsanker <alexanderforsanker@MacBook-Pro-som-tillhor-Alexander.local>
2023-03-26 23:25:51 +0200

    Added multi language support and fixed so the map view is latest location the phone was at.

--- app/src/main/java/se/example/mushroommapper/view/MapScreen.kt
+++ app/src/main/java/se/example/mushroommapper/view/MapScreen.kt
@@ -3,14 +3,18 @@ package se.example.mushroommapper.view
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import se.example.mushroommapper.LocationViewModel
 import se.example.mushroommapper.model.Places
 import se.example.mushroommapper.viewModel.HomeViewModel
 
@@ -32,33 +36,44 @@ fun MapScreen(
     for(place in homeUIState.placesList.data ?: emptyList()){
         places.add(Places(place.userId,place.title, place.description,place.latitude,place.longitude))
     }
-    //val mostRecentPosition
-
+    val locationViewModel = viewModel(modelClass = LocationViewModel::class.java)
+    val location by locationViewModel.getLocationLiveData().observeAsState()
     val singapore = LatLng(1.35, 103.87)
-    /*val l1 = "1.30"
-    val l2 = "103.84"
-    val places = listOf(
-        Places("Singapore","Nice city", "cool city",1.36,103.87),
-        Places("Singapore2","Nice city", "cool city",1.39,103.87),
-        Places("Singapore3","Nice city", "cool city",1.42,103.87),
-        Places("Singapore4","Nice city", "cool city",l1.toDouble(),l2.toDouble()),
-    )*/
+    val lat = location?.longitude?.toDoubleOrNull()
+    val lng = location?.longitude?.toDoubleOrNull()
 
-
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
-
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        for(place in places) {
-            Marker(
-                state = MarkerState(position = LatLng(place.latitude, place.longitude)),
-                title = place.title,
-                snippet = place.description,
-            )
+    if (lat != null && lng != null) {
+        val latestLocation = LatLng(lat, lng)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(latestLocation, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            for(place in places) {
+                Marker(
+                    state = MarkerState(position = LatLng(place.latitude, place.longitude)),
+                    title = place.title,
+                    snippet = place.description,
+                )
+            }
+        }
+    } else {
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            for(place in places) {
+                Marker(
+                    state = MarkerState(position = LatLng(place.latitude, place.longitude)),
+                    title = place.title,
+                    snippet = place.description,
+                )
+            }
         }
     }
 }
